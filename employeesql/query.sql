0a29fe2bb54592c1d686584788659dc51845df7b
Matt <mtfahys@gmail.com>
2020-02-04 12:50:00 -0500

    Update query.sql

--- employeesql/query.sql
+++ employeesql/query.sql
@@ -20,3 +20,29 @@ LEFT JOIN "EmploymentStatus" ON
 "Employees".emp_no="EmploymentStatus".emp_no
 LEFT JOIN "Departments" ON
 "EmploymentStatus".dept_no="Departments".dept_no;
+
+SELECT *
+FROM "Employees"
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+SELECT  dept_name, first_name, last_name, "Employees".emp_no
+FROM "Employees"
+LEFT JOIN "EmploymentStatus" ON 
+"Employees".emp_no="EmploymentStatus".emp_no
+LEFT JOIN "Departments" ON
+"EmploymentStatus".dept_no="Departments".dept_no
+WHERE "Departments".dept_no = 'd007';
+
+SELECT  dept_name, first_name, last_name, "Employees".emp_no
+FROM "Employees"
+LEFT JOIN "EmploymentStatus" ON 
+"Employees".emp_no="EmploymentStatus".emp_no
+LEFT JOIN "Departments" ON
+"EmploymentStatus".dept_no="Departments".dept_no
+WHERE "Departments".dept_no = 'd007'
+OR "Departments".dept_no = 'd005';
+
+SELECT last_name, count(emp_no)
+FROM "Employees"
+GROUP BY last_name;
